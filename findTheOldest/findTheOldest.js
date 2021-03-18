0ab7e192eae71098fcb65c0dc7373b42aeffe799
Jose Alejandro Figueras <68606242+freakAyLl@users.noreply.github.com>
2021-03-18 00:17:32 -0300

    Add a posible solution :)

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(peopleArr) {
+    peopleArr.sort((a,b)=>{
+        if(a.yearOfDeath===undefined){
+            a.yearOfDeath=2020
+        }
+        if(b.yearOfDeath===undefined){
+            b.yearOfDeath=2020
+        }
+        if(a.yearOfDeath-a.yearOfBirth> b.yearOfDeath-b.yearOfBirth){
+            return -1;
+        }
+        if(a.yearOfDeath-a.yearOfBirth< b.yearOfDeath-b.yearOfBirth){
+            return 1;
+        }
+        return 0
+    })
+    return peopleArr[0]
 }
-
 module.exports = findTheOldest
